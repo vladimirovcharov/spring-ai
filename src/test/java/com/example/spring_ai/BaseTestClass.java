8b8ac20e32d4ecfb9d9566bb23b4e12b9cdde697
Volodymyr Ovcharov <vladimir.1527@gmail.com>
2024-12-12 18:52:59 -0600

    Add giving of clear instructions

---
+++ src/test/java/com/example/spring_ai/BaseTestClass.java
@@ -0,0 +1,22 @@
+package com.example.spring_ai;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BaseTestClass {
+
+    @Autowired
+    ChatModel chatModel;
+
+    String chat(String prompt) {
+        PromptTemplate promptTemplate = new PromptTemplate(prompt);
+        Prompt promptToSend = promptTemplate.create();
+
+        return chatModel.call(promptToSend).getResult().getOutput().getContent();
+    }
+
+}
