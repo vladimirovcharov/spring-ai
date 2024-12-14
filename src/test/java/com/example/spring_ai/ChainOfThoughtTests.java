afdb83189aa1d6b40ab56dcf4c08e3d9472dfb15
Volodymyr Ovcharov <vladimir.1527@gmail.com>
2024-12-14 15:53:06 -0600

    Add chain of thought

---
+++ src/test/java/com/example/spring_ai/ChainOfThoughtTests.java
@@ -0,0 +1,59 @@
+package com.example.spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+
+public class ChainOfThoughtTests extends BaseTestClass {
+
+    @Test
+    void testTraditionalPrompt() {
+        String prompt = """
+                Q: Roger has 5 tennis balls. He buys 2 more cans of tennis balls, each containing 3 balls. \s
+                How many tennis balls does Roger have now?
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(prompt);
+
+        ChatResponse response = chatModel.call(promptTemplate.create());
+
+        System.out.println(response.getResult().getOutput().getContent());
+    }
+
+    /*
+     Chain of thought - adding a series of intermediate reasoning steps to the prompt.
+     See - https://arxiv.org/abs/2201.11903
+    */
+    @Test
+    void testChainOfThroughPrompt() {
+        String chainOfThoughtPrompt = """
+                Q: Roger has 5 tennis balls. He buys 2 more cans of tennis balls, each containing 3 balls. \s
+                How many tennis balls does Roger have now?
+                
+                A: Roger started with 5 balls. 2 cans of 3 balls each is 6 balls. 5 + 6 = 11. So Roger has 11 tennis balls.
+                
+                Q: The cafeteria had 23 apples originally. They used 20 apples to make lunch and bought 6 more. How many \s
+                apples does the cafeteria have now?
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(chainOfThoughtPrompt);
+
+        ChatResponse response = chatModel.call(promptTemplate.create());
+
+        System.out.println(response.getResult().getOutput().getContent());
+    }
+
+    @Test
+    void testTraditionalPrompt2() {
+        String prompt = """
+                Q: Roger has 5 tennis balls. He buys 2 more cans of tennis balls, each containing 3 balls. \s
+                How many tennis balls does Roger have now? Answer in 1 word.
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(prompt);
+
+        ChatResponse response = chatModel.call(promptTemplate.create());
+
+        System.out.println(response.getResult().getOutput().getContent());
+    }
+}
